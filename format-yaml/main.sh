9dfdf2a97a0a826f1ab850ebaadb8ebf2a6007c3
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2023-11-14 19:02:41 -0800

    yq stuff working

--- format-yaml/main.sh
+++ format-yaml/main.sh
@@ -8,12 +8,13 @@ IFS=$'\n\t'
 # - copy yamllint config
 cp "/Users/bbkane/Git-GH/example-go-cli/.yamllint.yml" .
 
-# - update lefthook and ci
+# - update lefthook
 yq -i '.pre-commit.commands.yamllint = {"run": "yamllint"}' lefthook.yml
 
-# TODO: this isn't working when copy-pasted into fling dir. See https://mikefarah.gitbook.io/yq/operators/add#append-to-existing-array and figure it out
-yq -i '.jobs.steps + {"name": "Install yamllint", "run: "pip install yamllint"}' .github/workflows/ci.yml
-# yq -i '.jobs.steps += {"name": "Lint YAML files", "run: "yamllint ."}' .github/workflows/ci.yml
+# - update ci
+# https://mikefarah.gitbook.io/yq/operators/add#append-to-existing-array and figure it out
+yq -i '.jobs.setup.steps += {"name": "Install yamllint", "run": "pip install yamllint"}' .github/workflows/ci.yml
+yq -i '.jobs.setup.steps += {"name": "Lint YAML files", "run": "yamllint ."}' .github/workflows/ci.yml
 
 # - run yq to format
 # - run yamllint? Or let CI do that?
